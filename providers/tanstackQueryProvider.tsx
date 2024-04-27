2c686128a29524e21df0c3b935e490456e30c2ad
primado <primadoprincepa@gmail.com>
2024-04-27 23:08:11 +0000

    add dev dependencies

---
+++ providers/tanstackQueryProvider.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React, { useState } from 'react'
+
+
+export default function TanstackQueryProvider({children}: {children: React.ReactNode}) {
+
+    const [ queryClient ] = useState(() => new QueryClient())
+
+    return (
+       <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false}/>
+       </QueryClientProvider>
+    )
+};
